cb55161f4c20540f388e2fefcb0498626cb2421b
Léo Verger <leo.verger@epitech.eu>
2023-10-09 17:05:53 +0200

    begin of data seed

---
+++ prisma/migrations/20231009145412_/migration.sql
@@ -0,0 +1,35 @@
+/*
+  Warnings:
+
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `applications` DROP FOREIGN KEY `applications_userId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `companies` DROP FOREIGN KEY `companies_userId_fkey`;
+
+-- DropTable
+DROP TABLE `users`;
+
+-- CreateTable
+CREATE TABLE `user` (
+    `userId` INTEGER NOT NULL AUTO_INCREMENT,
+    `firstName` VARCHAR(191) NOT NULL,
+    `lastName` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `userPassword` VARCHAR(191) NOT NULL,
+    `isAdmin` BOOLEAN NOT NULL DEFAULT false,
+    `exp` VARCHAR(191) NOT NULL,
+    `school` VARCHAR(191) NOT NULL,
+    `skills` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`userId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `companies` ADD CONSTRAINT `companies_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`userId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `applications` ADD CONSTRAINT `applications_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`userId`) ON DELETE SET NULL ON UPDATE CASCADE;
