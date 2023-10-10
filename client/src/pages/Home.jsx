61260b7475f7442baf3ed060286c67cf6e35d9d1
Alexandre.T <alexandre.tressel@icloud.com>
2023-10-10 10:35:17 +0200

    feat: :tada: Add Filter button for ads

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,29 @@
-const Home = () => {
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    )
-}
+import React, { useState } from 'react';
 
-export default Home
+const tabs = ['Découvrir', 'Sauvegardées', 'Candidatées', 'Fermées', 'Annulées', 'Discover'];
+
+const Home = () => {
+  const [activeTab, setActiveTab] = useState('Découvrir');
+
+  return (
+    <div className="container mx-auto px-4 mt-5">
+      <h1 className="text-3xl">Trouver un job</h1>
+      <div className="flex items-center rounded-full p-2 overflow-auto gap-4 relative">
+        <div className="absolute left-6 w-[600px] md:w-[700px] lg:w-[900px] xl:w-[1200px] h-4 bg-base-300 z-10"></div>
+        {tabs.map((tab, index) => (
+          <div
+            key={index}
+            onClick={() => setActiveTab(tab)}
+            className={`flex items-center md:w-1/6 rounded-full p-3 py-2 justify-center z-30 transition-colors duration-300 hover:bg-violet-700 hover:text-white active:text-white active:bg-violet-700 ease-in-out ${
+              activeTab === tab ? 'bg-violet-600 text-white' : 'bg-base-300'
+            }`}
+          >
+            <span>{tab}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
