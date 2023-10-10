046a75f25df311e6be923936027d7f48c9eea71f
Alexandre.T <alexandre.tressel@icloud.com>
2023-10-10 17:42:56 +0200

    feat: :sparkles: Add login/register form

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Home from "./pages/Home";
 import Layout from "./components/layouts/Layout";
 import { themeChange } from 'theme-change'
 import { useEffect } from "react";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
 
@@ -14,6 +16,8 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </Layout>
     </>
