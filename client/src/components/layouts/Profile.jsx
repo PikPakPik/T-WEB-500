0ccfc86f611a267db4f00761d8fbbf62aab2a240
Alexandre.T <alexandre.tressel@icloud.com>
2023-10-10 16:49:49 +0200

    Put by default not logged menu

--- client/src/components/layouts/Profile.jsx
+++ client/src/components/layouts/Profile.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ProfileDropdown = () => {
   return (
@@ -6,7 +7,7 @@ const ProfileDropdown = () => {
       <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
           <img
-            src="https://imgur.com/mfutu50.png"
+            src="https://cdn-icons-png.flaticon.com/256/3237/3237447.png"
             alt="avatar"
           />
         </div>
@@ -16,13 +17,7 @@ const ProfileDropdown = () => {
         className="mt-3 p-2 shadow menu menu-sm dropdown-content rounded-box w-52 bg-base-200 relative z-50"
       >
         <li>
-          <a>Profile </a>
-        </li>
-        <li>
-          <a>Settings</a>
-        </li>
-        <li>
-          <a>Logout</a>
+          <Link to="/login" className="flex items-center gap-2">Se connecter</Link>
         </li>
       </ul>
     </div>
