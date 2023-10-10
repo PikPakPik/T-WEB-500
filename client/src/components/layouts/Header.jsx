8bf04ffd89e530b5347c4f30c76fff8a9143a35b
Alexandre.T <alexandre.tressel@icloud.com>
2023-10-10 17:47:59 +0200

    Add link to main page and remove name for not logged user

--- client/src/components/layouts/Header.jsx
+++ client/src/components/layouts/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import ProfileDropdown from "./Profile";
 
 const { default: ThemeChange } = require("./ThemeChange");
@@ -5,7 +6,7 @@ const { default: ThemeChange } = require("./ThemeChange");
 const Header = () => {
   return (
     <div className="flex flex-row justify-between m-5">
-      <h1 className="text-3xl">Salut Alexandre 👋</h1>
+      <Link to="/"><h1 className="text-3xl">Salut 👋</h1></Link>
       <div className="flex flex-row gap-4">
         <ThemeChange />
         <ProfileDropdown />
