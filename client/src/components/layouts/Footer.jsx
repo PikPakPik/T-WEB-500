9636ad02ff089ff1292b9156415df7333c99d9d7
Alexandre.T <alexandre.tressel@icloud.com>
2023-10-10 14:36:27 +0200

    style: :lipstick: Add margin-top to have space

--- client/src/components/layouts/Footer.jsx
+++ client/src/components/layouts/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="footer footer-center p-4 bg-base-200 text-base-content">
+    <footer className="footer footer-center p-4 bg-base-200 text-base-content mt-11">
       <aside>
         <p>Copyright © 2023 - All right reserved by Epitech Nantes</p>
       </aside>
